64d437421c4c9e6e5cb9ff1a3d70a24993c8dbb5
Raghav-0007 <rmyadav514@gmail.com>
2025-02-23 20:53:48 +0530

    [Radhamohan yadav] . add created user model for greeting app

--- greetingapp/src/main/java/com/bridgelabz/greetingapp/controller/GreetingController.java
+++ greetingapp/src/main/java/com/bridgelabz/greetingapp/controller/GreetingController.java
@@ -1,10 +1,9 @@
 package com.bridgelabz.greetingapp.controller;
 
+import com.bridgelabz.greetingapp.model.User;
 import com.bridgelabz.greetingapp.service.GreetService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/greet")
@@ -17,5 +16,13 @@ public class GreetingController {
     @GetMapping("/hello")
     public String sayHello(){
         return greetService.sayHello();
+    }
+
+    // create post request to get user data
+    @PostMapping("/user")
+    public String sayHelloWithName(@RequestBody User user){
+        String message= "Hello "+user.getFirstName()+" "+ user.getLastName();
+        user.setMessage(message);
+        return user.getMessage();
     }
 }
